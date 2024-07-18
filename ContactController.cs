b10162f0e53566aad1be6a3cb0e85ee00a20f0c3
Atharva <atharva.nijasure2003@gmail.com>
2024-07-18 14:07:36 -0500

    Update and read functioning

--- ContactController.cs
+++ ContactController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Spectre.Console;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,23 @@ namespace ConsoleAppContacts
     {
         internal static void AddContact()
         {
-            throw new NotImplementedException();
+            var fName = AnsiConsole.Ask<string>("What's the contact's first name?");
+            var lName = AnsiConsole.Ask<string>("What's the contact's last name?");
+            var email = AnsiConsole.Ask<string>("What's the email?");
+            var phoneNumber = AnsiConsole.Ask<string>("What's the phone number?");
+            Contact contact = new Contact
+            {
+                FirstName = fName,
+                LastName = lName,
+                Email = email,
+                PhoneNumber = phoneNumber,
+            };
+
+            using (var context = new ContactsContext())
+            {
+                context.Contacts.Add(contact);
+                context.SaveChanges();
+            }
         }
 
         internal static void DeleteContact()
@@ -26,6 +43,24 @@ namespace ConsoleAppContacts
         internal static void UpdateContact()
         {
             throw new NotImplementedException();
+        }
+
+        internal static void ReadContacts()
+        {
+            Table table = new Table();
+            table.AddColumn("First Name");
+            table.AddColumn("Last Name");
+            table.AddColumn("Email");
+            table.AddColumn("Phone Number");
+            using (var context = new ContactsContext())
+            {
+                var contactList = context.Contacts.ToList();
+                foreach(Contact currContact in contactList)
+                {
+                    table.AddRow(currContact.FirstName, currContact.LastName, currContact.Email, currContact.PhoneNumber);
+                }
+            }
+            AnsiConsole.Write(table);
         }
     }
 }
