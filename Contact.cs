a9b668d8690ec397607ed5047da41bfbbcfc1289
Atharva <atharva.nijasure2003@gmail.com>
2024-07-18 13:28:52 -0500

    Add data validation

--- Contact.cs
+++ Contact.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ConsoleAppContacts
 {
     public class Contact
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
         public int Id { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [Phone]
         public string PhoneNumber { get; set; }
 
     }
